00ac8f51e78dc8390c95907653359af8dc68aab4
savvatto <svvatto9121999@gmail.com>
2020-08-11 18:09:28 +0900

    feedout実装済み（メソッドにはしてない）

---
+++ Assets/FadeOut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    AudioSource audioSource;
+    public bool IsFade;
+    public double FadeOutSeconds = 1.0;
+    bool IsFadeOut = true;
+    double FadeDeltaTime = 0;
+
+    void Start()
+    {
+        audioSource = GetComponent <AudioSource> ();
+    }
+
+    void Update()
+    {
+        if (IsFadeOut)
+        {
+            FadeDeltaTime += Time.deltaTime ;
+            if (FadeDeltaTime >= FadeOutSeconds)
+            {
+                FadeDeltaTime = FadeOutSeconds;
+                IsFadeOut = false;
+            }
+            audioSource.volume = (float)(1.0 - FadeDeltaTime / FadeOutSeconds);
+        }
+    }
+}
