f76d5e821727826c836bfea9d84a031c76927398
savvatto <svvatto9121999@gmail.com>
2020-08-13 00:00:51 +0900

    完成したと思われる（テスト用のstart関数はまだ消してない）

--- Assets/FadeOut.cs
+++ Assets/FadeOut.cs
@@ -1,31 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.Audio;
+
 
 public class FadeOut : MonoBehaviour
 {
     AudioSource audioSource;
-    public bool IsFade;
-    public double FadeOutSeconds = 1.0;
+    public AudioClip bgmClip;
     bool IsFadeOut = true;
+    double FadeOutSeconds = 3.0;
     double FadeDeltaTime = 0;
 
+    //この関数は実際には使用しない。テストで使用しているため、あとで消す。
     void Start()
     {
-        audioSource = GetComponent <AudioSource> ();
+        //BGMsystem関数の引数に次に流すBGMのファイル名を入れればフェードアウトして切り替わる
+        BGMsystem("test2");
     }
 
-    void Update()
+    public void BGMsystem(string bgmName)
     {
+        audioSource = GetComponent<AudioSource>();
+        fadeout();
+
+        StartCoroutine(DelayMethod((float)FadeOutSeconds, () =>
+        {
+            cBGM(bgmName);
+        }));
+    }
+
+    public void fadeout()
+    {
+        //変数初期化
+        IsFadeOut = true;
+        FadeOutSeconds = 3.0;
+        FadeDeltaTime = 0;
+        //1fごとにループするフェード処理
+        StartCoroutine("Fade");
+    }
+
+    IEnumerator Fade()
+    {
+        FadeDeltaTime += Time.deltaTime;
+        if (FadeDeltaTime >= FadeOutSeconds)
+        {
+            FadeDeltaTime = FadeOutSeconds;
+            IsFadeOut = false;
+        }
+        audioSource.volume = (float)(1.0 - FadeDeltaTime / FadeOutSeconds);
+        //1フレーム遅らせる
+        yield return null;
+        //FadeOutSeconds未満の秒数経過で再起に入る
         if (IsFadeOut)
         {
-            FadeDeltaTime += Time.deltaTime ;
-            if (FadeDeltaTime >= FadeOutSeconds)
-            {
-                FadeDeltaTime = FadeOutSeconds;
-                IsFadeOut = false;
-            }
-            audioSource.volume = (float)(1.0 - FadeDeltaTime / FadeOutSeconds);
+            StartCoroutine("Fade");
         }
     }
+
+    void cBGM(string bgmName)
+    {
+        if (audioSource.clip.name == bgmName)
+        {
+            // すでに再生中なら変えない
+            return;
+        }
+        //Resources内に音源を入れないとResourcesは動かないため、フォルダ構造は要注意IsFadeOut)
+        bgmClip = Resources.Load<AudioClip>("BGM/" + bgmName);
+        audioSource.clip = bgmClip;
+        audioSource.Stop();
+        audioSource.volume = 1;
+        audioSource.Play();
+    }
+
+    IEnumerator DelayMethod(float waitTime, Action action)
+    {
+        //チェンジ処理をフェードアウトに必要な秒数だけ遅らせる
+        yield return new WaitForSeconds(waitTime);
+        action();
+    }
+
 }
