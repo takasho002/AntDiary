5e05b686fe2407ca1873d3c6b7569bd2db35bcfe
savvatto <svvatto9121999@gmail.com>
2020-08-15 12:57:44 +0900

    仮でantクラスを作成して、衝突判定を作成。オブジェクトはbox コライダー2Dで作成しているため、相談後必要なら修正。デバッグできていない。

---
+++ Assets/SugarSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SugarSystem : MonoBehaviour
+{
+    public double Amount_of_sugar = 0;
+    public float Season_coefficient = 1.0f;
+    public float Ant_ability_sugar = 2.0f;
+    Ant ant;
+    // トリガーとの接触時に呼ばれるコールバック
+    void OnTriggerEnter(Collider hit)
+    {
+        // 接触対象がSugarタグを持つオブジェクトだった場合に処理をする
+        // ※ ant同士で接触して砂糖が得られないようにする
+        if (hit.CompareTag("Sugar"))
+        {
+            // 砂糖を持たせる
+            ant.HavingSugar = Math.Ceiling(Season_coefficient * ant.SugarAbility);
+
+        }
+    }
+}
+public class Ant : MonoBehaviour
+{
+    public double HavingSugar = 0;
+    public float SugarAbility = 2.0f;
+
+}
