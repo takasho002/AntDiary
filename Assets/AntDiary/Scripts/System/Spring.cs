ee9feb0e76b6e13cd53a974f6cf572b65cfa7ae7
kazsource <cultotto@gmail.com>
2020-08-16 15:29:06 +0900

    春と冬をテキスト出力できました

---
+++ Assets/AntDiary/Scripts/System/Spring.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AntDiary;
+
+public class Spring : MonoBehaviour
+{
+
+    public Text Springtext;
+    private int counttime;
+
+    private float season;
+    private void Start()
+    {
+      season=0;
+      Springtext.text = "";  
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        season = TimeSystem.Instance.CurrentSeason;
+        Springcheck();
+    }
+
+    private void Springcheck()
+    {
+        if(season==3){
+            Springtext.text = "Spring";
+        }
+    } 
+}
---
+++ Assets/AntDiary/Scripts/System/Winter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AntDiary;
+
+public class Winter : MonoBehaviour
+{
+
+    public Text Wintertext;
+    private int counttime;
+
+    private float season;
+    private void Start()
+    {
+      season=0;
+      Wintertext.text = "";  
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        season = TimeSystem.Instance.CurrentSeason;
+        Wintercheck();
+    }
+
+    private void Wintercheck()
+    {
+        if(season==0){
+            Wintertext.text = "Winter";
+        }
+    } 
+}
