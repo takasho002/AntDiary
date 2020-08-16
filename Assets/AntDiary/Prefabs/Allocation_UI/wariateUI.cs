b1e629e715e7b6549ffcddbeca9975fd10e135da
sayoshi <buromasa1126@gmail.com>
2020-08-17 01:49:56 +0900

    割り当てUIの背景と棒グラフの部分が完成しました。(例棒グラフの赤の割合を増やすとした二つの棒グラフの灰色の部分が増加するような具合の動きができるようになりました)

---
+++ Assets/AntDiary/Prefabs/Allocation_UI/wariateUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class wariateUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    //public Transform parentTransform;
+    public Image scrollbar;
+    public Image grayScrollbar;
+    public Image otherScrollbar1;
+    public Image otherScrollbar2;
+    public float Max;
+    void start()
+    {
+
+    }
+
+    public void Update()
+    {
+        grayScrollbar.fillAmount = otherScrollbar1.fillAmount + otherScrollbar2.fillAmount;
+        Max = (1 - grayScrollbar.fillAmount) * 400-200;
+        if (transform.localPosition.x>Max)
+        {
+            transform.localPosition = new Vector3(Max,transform.localPosition.y,transform.localPosition.z) ;
+        }
+        else if(transform.localPosition.x<-200)
+        {
+            transform.localPosition = new Vector3(-200, transform.localPosition.y, transform.localPosition.z);
+        }
+        scrollbar.fillAmount = (transform.localPosition.x + 200) / 400;
+    }
+    public void OnBeginDrag(PointerEventData data)//ドラッグはじめ
+    {
+        Debug.Log("OnBeginDrag");
+    }
+    public void OnDrag(PointerEventData data)//ドラッグ中
+    {
+        transform.position = new Vector3(data.position.x,transform.position.y,transform.position.z);
+        Debug.Log(transform.localPosition);
+    }
+    public void OnEndDrag(PointerEventData data)//ドラッグ終わり
+    {
+        Debug.Log("OnEndDrag");
+    }
+}
