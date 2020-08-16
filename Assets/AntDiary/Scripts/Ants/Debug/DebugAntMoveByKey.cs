e38ba46d3512de215d575ecd56bb7c0f048237fe
furi_RiF <t.15.furi@gmail.com>
2020-08-16 17:49:03 +0900

    貯蓄庫に貯蓄量を保持するグローバル変数を追加 デバッグ用に餌を持つアリを円オブジェクトで実装 餌を貯蓄庫に移動させるアルゴリズムをアリのオブジェクトに追加

---
+++ Assets/AntDiary/Scripts/Ants/Debug/DebugAntMoveByKey.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugAntMoveByKey : MonoBehaviour
+{
+    public GameObject targetObj;
+    public int feedAmount;
+    public bool isHoldingFood;
+
+    private Chochikuko chochikuko;
+    private Vector3 targetPos;
+    private Vector3 nowPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nowPos = targetObj.transform.position;
+        chochikuko = targetObj.GetComponent <Chochikuko>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-0.1f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0.1f, 0);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -0.1f, 0);
+        }
+
+        targetPos = transform.position;
+        if (Vector3.Distance(targetPos, nowPos) < 1)
+        {
+            Debug.Log("touched");
+            if (isHoldingFood)
+            {
+                chochikuko.stockAmount += feedAmount;
+                feedAmount = 0;
+                isHoldingFood = false;
+            }
+        }
+    }
+}
---
+++ Assets/AntDiary/Scripts/System/Chochikuko.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chochikuko : MonoBehaviour
+{
+    //  貯蓄庫の在庫量をここで管理する
+    [HideInInspector] public int stockAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 貯蓄庫の初期の貯蓄量を0とする
+        stockAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(stockAmount);
+    }
+}
