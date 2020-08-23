6bab82824ad223fbbb8d67e1172eb9bb87ce8291
elecdeer <deersub@gmail.com>
2020-08-23 14:51:11 +0900

    TShapeの道を実装

--- Assets/AntDiary/Scripts/Roads/TShapeRoadFactory.cs
+++ Assets/AntDiary/Scripts/Roads/TShapeRoadFactory.cs
@@ -1,5 +1,37 @@
+using System.IO;
+using AntDiary.Scripts.Roads;
+using UnityEngine;
+
 namespace AntDiary.Scripts.Roads{
-	public class TShapeRoadFactory{
+	public class TShapeRoadFactory: NestElementFactory<TShapeRoadData>{
+		[SerializeField] private GameObject topDirRoadPrefab;
+		[SerializeField] private GameObject rightDirRoadPrefab;
+		[SerializeField] private GameObject bottomDirRoadPrefab;
+		[SerializeField] private GameObject leftDirRoadPrefab;
+
 		
+		public override NestElement InstantiateNestElement(TShapeRoadData elementData){
+			var prefab = GetPrefab(elementData);
+			var gameObj = Instantiate(prefab, elementData.Position, Quaternion.identity);
+			var nestElem = gameObj.GetComponent<TShapeRoad>();
+			nestElem.Initialize(elementData);
+
+			return nestElem;
+		}
+		
+		private GameObject GetPrefab(TShapeRoadData roadData){
+			switch(roadData.Direction){
+				case EnumRoadDirection.Top:
+					return topDirRoadPrefab;
+				case EnumRoadDirection.Right:
+					return rightDirRoadPrefab;
+				case EnumRoadDirection.Bottom:
+					return bottomDirRoadPrefab;
+				case EnumRoadDirection.Left:
+					return leftDirRoadPrefab;
+			}
+
+			throw new InvalidDataException();
+		}
 	}
 }
