a2e47a2babcff7f65c50400f399c6434b4358ab7
sayoshi <buromasa1126@gmail.com>
2020-08-23 13:40:45 +0900

    DragAndDrop.csに書き足したい内容があるのですが、衝突を避けるため同じ内容のスクリプトを作成しKenchikuUIシーン内のオブジェクトにつけなおしました

---
+++ Assets/AntDiary/Scripts/UI/DragAndDrop_ExOfBuildingSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace AntDiary
+{
+    public class DragAndDrop_ExOfBuildingSystem : MonoBehaviour
+    {
+        public GameObject root;
+        private GameObject rootObject;
+
+        private Vector3 position;
+        private Vector3 screenToWorldPointPosition;
+
+        public void PushDown()
+        {
+            position = Input.mousePosition;
+            position.z = 10f;
+            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+            rootObject = Instantiate(root, screenToWorldPointPosition, Quaternion.identity);
+
+        }
+
+        public void PushDrag()
+        {
+            position = Input.mousePosition;
+            position.z = 10f;
+            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+            rootObject.transform.position = screenToWorldPointPosition;
+        }
+    }
+}
