3488979d0be166494e50d7694d9beb6935dccb39
furi_RiF <t.15.furi@gmail.com>
2020-08-26 18:24:50 +0900

    日時UIで，現在時刻を時分秒で表示可能にした． 季節でフレームも変わるようにした．

---
+++ Assets/AntDiary/Scripts/UI/DateTimeUI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AntDiary
+{
+    public class DateTimeUI : MonoBehaviour
+    {
+        private TimeSystem TimeSystem => TimeSystem.Instance;
+
+        [SerializeField] private Text monthAndSeason = default;
+        [SerializeField] private Text timeData = default;
+        [SerializeField] private Sprite springFrame = default;
+        [SerializeField] private Sprite summerFrame = default;
+        [SerializeField] private Sprite autumnFrame = default;
+        [SerializeField] private Sprite winterFrame = default;
+
+        private Image image;
+
+        void Start()
+        {
+            image = gameObject.GetComponent<Image>();
+
+            getSeasonSprite(TimeSystem.CurrentSeason);
+            timeData.text = getCurrentTimeData(TimeSystem.CurrentTime);
+        }
+
+        void Update()
+        {
+            getSeasonSprite(TimeSystem.CurrentSeason);
+            timeData.text = getCurrentTimeData(TimeSystem.CurrentTime);
+        }
+
+        private void getSeasonSprite(int seasonId)
+        {
+            switch(seasonId)
+            {
+                case 0:
+                    image.sprite = springFrame;
+                    break;
+                case 1:
+                    image.sprite = summerFrame;
+                    break;
+                case 2:
+                    image.sprite = autumnFrame;
+                    break;
+                case 3:
+                    image.sprite = winterFrame;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private string getCurrentTimeData(float currentTime)
+        {
+            int hour = (int)currentTime / 3600;
+            int minute = (int)currentTime / 60;
+            int second = (int)currentTime % 60;
+            string time = hour.ToString("00") + ":";
+            time += minute.ToString("00") + ":";
+            time += second.ToString("00");
+
+            return time;
+        }
+    }
+}
