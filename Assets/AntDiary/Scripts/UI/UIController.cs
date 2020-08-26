9a7bc8a0d11c45ef42c49a77a5b5a2a14d57a8a7
DESKTOP-2VQTDIS\syogu <syogunn519@gmail.com>
2020-08-26 18:03:06 +0900

    UI切り替えシステム完了 配置とボタンのスプライトは変更する必要性あり 他のUIシステムの実装待ち

---
+++ Assets/AntDiary/Scripts/UI/UIController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] GameObject[] panels;
+    [SerializeField] GameObject[] buttons;
+    [SerializeField] Sprite inactive;
+    [SerializeField] Sprite active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void TogglePanelActive(int i)
+    {
+        if (panels[i].activeSelf)
+        {
+            panels[i].SetActive(false);
+        }else{
+            for(int j = 0; j < panels.Length; j++)
+            {
+                panels[j].SetActive(false);
+            }
+            panels[i].SetActive(true);
+        }
+    }
+
+    private void ToggleButtonImage(int i)
+    {
+        if (panels[i].activeSelf)
+        {
+            buttons[i].GetComponent<Image>().sprite = inactive;
+        }
+        else
+        {
+            for (int j = 0; j < buttons.Length; j++)
+            {
+                buttons[j].GetComponent<Image>().sprite = inactive;
+            }
+            buttons[i].GetComponent<Image>().sprite = active;
+        }
+    }
+
+    public void ToggleUIandButton(int i)
+    {
+        ToggleButtonImage(i);
+        TogglePanelActive(i);
+    }
+}
