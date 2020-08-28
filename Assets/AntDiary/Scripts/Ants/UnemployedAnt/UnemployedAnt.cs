d79cd3abf2dfe92e00332b20816b3e53a861509b
Kota Ninomiya <uv.ninomiya@gmail.com>
2020-08-28 15:48:07 +0900

    無職アリに関する一式のプログラム

---
+++ Assets/AntDiary/Scripts/Ants/UnemployedAnt/UnemployedAnt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AntDiary
+{ 
+    public class UnemployedAnt : Ant<UnemployedAntData>
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("UnemployedAnt spawned");
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+        // DebugAntそのまま
+        protected override float MovementSpeed { get; } = 1f;
+    }
+}
