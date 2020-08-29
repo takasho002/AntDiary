3c780f542795d9bfc1b0ccb01f5ff4bae348e5bb
elecdeer <deersub@gmail.com>
2020-08-30 00:01:16 +0900

    テスト用のマップを作った

--- Assets/AntDiary/Scripts/System/TestRoadInstantiate.cs
+++ Assets/AntDiary/Scripts/System/TestRoadInstantiate.cs
@@ -9,6 +9,76 @@ public class TestRoadInstantiate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start(){
+        NestElement[,] map = new NestElement[5,6];
+        
+       
+
+        map[0, 0] = InstantiateLShapeRoad(EnumRoadDirection.Right);
+        map[0, 1] = InstantiateLShapeRoad(EnumRoadDirection.Bottom);
+        map[0, 2] = InstantiateIShapeRoad(EnumRoadHVDirection.Horizontal);
+        map[0, 3] = InstantiateLShapeRoad(EnumRoadDirection.Bottom);
+
+        map[1, 0] = InstantiateIShapeRoad(EnumRoadHVDirection.Vertical);
+        map[1, 1] = InstantiateTShapeRoad(EnumRoadDirection.Right);
+        map[1, 2] = InstantiateLShapeRoad(EnumRoadDirection.Bottom);
+        map[1, 3] = InstantiateTShapeRoad(EnumRoadDirection.Right);
+        map[1, 4] = InstantiateIShapeRoad(EnumRoadHVDirection.Horizontal);
+        map[1, 5] = InstantiateLShapeRoad(EnumRoadDirection.Bottom);
+
+        map[2, 0] = InstantiateIShapeRoad(EnumRoadHVDirection.Vertical);
+        map[2, 1] = InstantiateTShapeRoad(EnumRoadDirection.Right);
+        map[2, 2] = InstantiateCrossShapeRoad();
+        map[2, 3] = InstantiateLShapeRoad(EnumRoadDirection.Left);
+        map[2, 4] = InstantiateLShapeRoad(EnumRoadDirection.Right);
+        map[2, 5] = InstantiateLShapeRoad(EnumRoadDirection.Left);
+
+        map[3, 0] = InstantiateLShapeRoad(EnumRoadDirection.Top);
+        map[3, 1] = InstantiateTShapeRoad(EnumRoadDirection.Top);
+        map[3, 2] = InstantiateCrossShapeRoad();
+        map[3, 3] = InstantiateIShapeRoad(EnumRoadHVDirection.Horizontal);
+        map[3, 4] = InstantiateCrossShapeRoad();
+        map[3, 5] = InstantiateLShapeRoad(EnumRoadDirection.Left);
+
+        map[4, 2] = InstantiateLShapeRoad(EnumRoadDirection.Top);
+        map[4, 3] = InstantiateIShapeRoad(EnumRoadHVDirection.Horizontal);
+        map[4, 4] = InstantiateTShapeRoad(EnumRoadDirection.Top);
+        map[4, 5] = InstantiateLShapeRoad(EnumRoadDirection.Bottom);
+
+        for(int i = 0; i < map.GetLength(0); i++){
+            for(int j = 0; j < map.GetLength(1); j++){
+                if(map[i, j] == null){
+                    continue;
+                }
+                map[i, j].transform.position -= new Vector3((j-3)*-4, (i-3)*4);
+            }
+        }
+
+       
+
+        //横接続
+        for(int i = 0; i < map.GetLength(0); i++){
+            for(int j = 0; j < map.GetLength(1) - 1; j++){
+                var elemA = map[i, j]?.GetNodes().First(n => n.Name == "right");
+                var elemB = map[i, j+1]?.GetNodes().First(n => n.Name == "left");
+                if(elemA != null && elemB != null){
+                    NestSystem.Instance.ConnectElements(elemA, elemB);
+                }
+            }
+        }
+        
+        //縦接続
+        for(int i = 0; i < map.GetLength(0) - 1; i++){
+            for(int j = 0; j < map.GetLength(1); j++){
+                var elemA = map[i, j]?.GetNodes().First(n => n.Name == "top");
+                var elemB = map[i+1, j]?.GetNodes().First(n => n.Name == "bottom");
+                if(elemA != null && elemB != null){
+                    NestSystem.Instance.ConnectElements(elemA, elemB);
+                }
+            }
+        }
+        
+        
+        
         
         //IShape
         // Debug.Log("Horizontal");
@@ -38,7 +108,7 @@ public class TestRoadInstantiate : MonoBehaviour
 
         
         //Cross
-        var roadH = InstantiateCrossShapeRoad();
+        // var roadH = InstantiateCrossShapeRoad();
         
 
         // roadB.transform.position = NestSystem.Instance.BuildingSystem.Instance.GetSnappedPosition(roadB);
