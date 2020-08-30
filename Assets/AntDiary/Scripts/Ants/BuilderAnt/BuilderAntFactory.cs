88fd3fb9b63085dc99210462452bc020ec3a67e6
elecdeer <deersub@gmail.com>
2020-08-30 16:22:26 +0900

    BuilderAntFactory書いてなかった

--- Assets/AntDiary/Scripts/Ants/BuilderAnt/BuilderAntFactory.cs
+++ Assets/AntDiary/Scripts/Ants/BuilderAnt/BuilderAntFactory.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 namespace AntDiary{
 	public class BuilderAntFactory : AntFactory<BuilderAntData>{
-		// Start is called before the first frame update
-		void Start(){
-        
-		}
-
-		// Update is called once per frame
-		void Update(){
-        
-		}
+		
+		[SerializeField] private GameObject antPrefab = default;
 
 		public override Ant InstantiateAnt(BuilderAntData antData){
-			throw new System.NotImplementedException();
+			var antObject = Instantiate(antPrefab, antData.Position, Quaternion.identity);
+			var ant = antObject.GetComponent<BuilderAnt>();
+			ant.Initialize(antData);
+			return ant;
 		}
 	}
 
