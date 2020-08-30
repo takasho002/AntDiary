00f0de6064bde9feafeaaa9b1d0a7c2320ca4c67
sayoshi <buromasa1126@gmail.com>
2020-08-31 03:31:12 +0900

    ゲームオーバー用のシーンをSayoshi_GameOverとして追加しました。 餌の総量が一定以下になったらゲームオーバーのシーンへ移行するFoodGameOverSystem.csを追加しました 10秒ごとに判定 餌の保存量が-50になったらシーンを遷移
    女王アリが死んだらゲームオーバーのシーンへ移行する予定のQweenGameOver.csを追加しました

---
+++ Assets/AntDiary/Scripts/System/QweenGameOverSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace AntDiary
+{
+    public class QweenGameOverSystem : MonoBehaviour
+    {
+        public AntData QweenAnt;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(QweenAnt.IsAlive == false)
+            {
+                SceneManager.LoadScene("Sayoshi_GameOver");
+            }
+        }
+    }
+}
