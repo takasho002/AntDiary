6259468827119ed34afb5e6d251db2b05e4a202b
DESKTOP-2VQTDIS\syogu <syogunn519@gmail.com>
2020-09-02 19:46:13 +0900

    女王アリのゲームオーバーシステムを女王アリにアタッチするよう変更、ゲームオーバー実装

--- Assets/AntDiary/Scripts/System/QweenGameOverSystem.cs
+++ Assets/AntDiary/Scripts/System/QweenGameOverSystem.cs
@@ -7,20 +7,20 @@ namespace AntDiary
 {
     public class QweenGameOverSystem : MonoBehaviour
     {
-        private DebugAntData QweenAnt;//DebugAntDataを、これから先QweenAntDataとかができたらそちらに修正する必要あり
-        public GameObject QweenObject;//ここにInspectorから女王バチのGameObjectをアタッチ(保存)する
+        private QueenAntData QueenAnt;//DebugAntDataを、これから先QweenAntDataとかができたらそちらに修正する必要あり
+        //public GameObject QueenObject;//ここにInspectorから女王バチのGameObjectをアタッチ(保存)する
         // Start is called before the first frame update
         void Start()
         {
-            QweenAnt = QweenObject.GetComponent<DebugAntData>();
+            QueenAnt = (QueenAntData)gameObject.GetComponent<QueenAnt>().Data;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if(QweenAnt.IsAlive == false)
+            if(QueenAnt.IsAlive == false)
             {
-                SceneManager.LoadScene("Sayoshi_GameOver");
+                SceneManager.LoadScene("GameOverScene");
             }
         }
     }
