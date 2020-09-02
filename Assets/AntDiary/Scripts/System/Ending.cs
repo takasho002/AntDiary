4205ea1b09cbc7feab430e8ccb89dc6387fdbc97
DESKTOP-2VQTDIS\syogu <syogunn519@gmail.com>
2020-09-02 15:19:49 +0900

    エンディングシーンまわりの遷移とゲームオーバーシーンの実装

---
+++ Assets/AntDiary/Scripts/System/Ending.cs
@@ -0,0 +1,24 @@
+﻿using AntDiary;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    private bool winter = false;
+    private int season => TimeSystem.Instance.CurrentSeason;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (season == 3) winter = true;
+        if (winter && season == 0) LoadEnding();
+    }
+
+    private void LoadEnding()
+    {
+        SceneManager.LoadScene("EndingScene");
+    }
+
+}
