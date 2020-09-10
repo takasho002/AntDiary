daad1c74b78e1555e84327ca893a5fc8c507ef99
DESKTOP-2VQTDIS\syogu <syogunn519@gmail.com>
2020-09-10 16:09:04 +0900

    アニメーション管理及びスプライト方向転換の作成

---
+++ Assets/AntDiary/Scripts/Ants/AnimatorAnt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorAnt : MonoBehaviour
+{
+    private bool walking = false;
+    private Vector2 prePos;
+    [SerializeField] private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prePos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prePos != (Vector2)transform.position && !walking)
+        {
+            walking = true;
+            animator.SetBool("Walking", walking);
+        }else if (prePos == (Vector2)transform.position && walking){
+            walking = false;
+            animator.SetBool("Walking", walking);
+        }
+        //進行方向への回転
+        if (walking)
+        {
+            var v = (Vector2)transform.position - prePos;
+            float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg; ;
+            var scale = transform.localScale;
+            var rotation = transform.eulerAngles;
+            Debug.Log(angle);
+            if (v.x > 0)
+            {
+                scale.x = -Math.Abs(scale.x);
+                rotation.z = angle;
+
+            }
+            else
+            {
+                scale.x = Math.Abs(scale.x);
+                rotation.z = angle - 180f;
+            }
+            transform.localScale = scale;
+            transform.eulerAngles = rotation;
+        }
+        prePos = transform.position;
+    }
+}
---
+++ Assets/AntDiary/Scripts/Ants/AnimatorStrategyAnt.cs
@@ -0,0 +1,63 @@
+﻿using AntDiary.Scripts.Ants;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AntDiary;
+using AntDiary.Scripts.Ants.SoldierAnt;
+using System;
+using System.Linq;
+using Boo.Lang.Runtime.DynamicDispatching;
+
+/// <summary>
+/// StrategyAnt用アニメーション管理のスクリプト
+/// 死亡アニメーションは死亡で管理
+/// </summary>
+public abstract class AnimatorStrategyAnt<T,TData> : MonoBehaviour where T: StrategyAnt<TData> where TData: StrategyAntData
+{
+    protected String preStrategy = "";
+    [SerializeField] protected Animator animator;
+    [SerializeField] protected T ant;
+
+    private Vector2 prePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        preStrategy = ant.GetCurrentStrategyName();
+        SetAnimation(preStrategy);
+        prePos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var nowStrategy = ant.GetCurrentStrategyName();
+        if (!preStrategy.Equals(nowStrategy)) SetAnimation(nowStrategy);
+        //進行方向への回転
+        if(prePos != (Vector2)transform.position)
+        {
+            var v = (Vector2)transform.position - prePos;
+            float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg; ;
+            var scale = transform.localScale;
+            var rotation = transform.eulerAngles;
+            Debug.Log(angle);
+            if (v.x > 0)
+            {
+                scale.x = -Math.Abs(scale.x);
+                rotation.z = angle;
+                
+            }
+            else
+            {
+                scale.x = Math.Abs(scale.x);
+                rotation.z = angle-180f;
+            }
+            transform.localScale = scale;
+            transform.eulerAngles = rotation;
+        }
+        prePos = (Vector2)transform.position;
+    }
+
+
+    protected abstract void SetAnimation(String nowStrategy);
+}
